a463a35e2a6f967a18fe63a874fdc3e1ef83aa23
Vijay <vijay.nagaraj2007@gmail.com>
2019-09-03 23:21:51 +0530

    Added autoconstant java file.

---
+++ src/com/gmail/generics/AutoConstant.java
@@ -0,0 +1,12 @@
+package com.gmail.generics;
+
+public interface AutoConstant 
+{
+	String chrome_key="webdriver.chrome.driver";
+	String chome_value="./drivers/chromedriver.exe";
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./drivers/geckodriver.exe";
+	String file_path="./data/input.xlsx";
+
+
+}
